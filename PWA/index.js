45cddd0ffaa3f48636ad2a53b741208a0b495337
gorenjak <gorenjak48@gmail.com>
2024-10-10 08:44:27 +0200

    test

--- PWA/index.js
+++ PWA/index.js
@@ -1,5 +1,5 @@
 // Import socket.io and initialize connection
-const socket = io('http://localhost:9000');
+const socket = io('https://test-5fdn.onrender.com:9000');
 
 // Connect to the socket.io server
 socket.on('connect', () => {
